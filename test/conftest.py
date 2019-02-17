ad005c07860b84fae3021b2acbd5990bf0b6cb51
Lauren Yu <6631887+laurenyu@users.noreply.github.com>
2019-02-17 11:34:56 -0800

    Fix instance_type fixture setup for tests

--- test/conftest.py
+++ test/conftest.py
@@ -98,8 +98,9 @@ def tag(request, toolkit, toolkit_version, processor):
 
 @pytest.fixture(scope='session')
 def instance_type(request, processor):
-    return request.config.getoption('--instance-type') or \
-        'ml.c4.xlarge' if processor == 'cpu' else 'ml.p2.xlarge'
+    provided_instance_type = request.config.getoption('--instance-type')
+    default_instance_type = 'ml.c4.xlarge' if processor == 'cpu' else 'ml.p2.xlarge'
+    return provided_instance_type if provided_instance_type is not None else default_instance_type
 
 
 @pytest.fixture(scope='session')
