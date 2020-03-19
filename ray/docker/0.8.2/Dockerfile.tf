f097447532173d60c31c98bc1a6ba7af15f2c6f5
Yijie Zhuang <remixzyj@gmail.com>
2020-03-19 16:32:29 -0700

    add Dockerfile for RLLib v0.8.2, add buildspec for RLLib container (#36)
    * add buildspec and Dockerfile for ray 0.8.2
    
    * add suffix to base images
    
    * add cpu integ tests
    
    * change train_ray.py to be compatible with new interface
    
    * add gpu integ tests
    
    * add ray[tune] and other dependencies
    
    * change remote test branch to 'master'

---
+++ ray/docker/0.8.2/Dockerfile.tf
@@ -0,0 +1,53 @@
+ARG processor
+ARG region
+ARG suffix
+
+FROM 763104351884.dkr.ecr.$region.amazonaws.com/tensorflow-training:2.1.0-$processor-py36-$suffix
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+        build-essential \
+        jq \
+        ffmpeg \
+        rsync \
+        libjpeg-dev \
+        libxrender1 \
+        python3.6-dev \
+        python3-opengl \
+        wget \
+        xvfb && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN pip install --upgrade pip
+
+RUN pip install --no-cache-dir \
+    Cython==0.29.7 \
+    tabulate \
+    tensorboardX \
+    gputil \
+    gym==0.12.1 \
+    lz4 \
+    opencv-python-headless==4.1.0.25 \
+    PyOpenGL==3.1.0 \
+    pyyaml \
+    redis==3.3.2 \
+    ray==0.8.2 \
+    ray[tune]==0.8.2 \
+    ray[rllib]==0.8.2 \
+    scipy \
+    psutil \
+    setproctitle \
+    tensorflow-probability
+
+# https://click.palletsprojects.com/en/7.x/python3/
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+
+# Copy workaround script for incorrect hostname
+COPY lib/changehostname.c /
+
+COPY lib/start.sh /usr/local/bin/start.sh
+RUN chmod +x /usr/local/bin/start.sh
+
+# Starts framework
+ENTRYPOINT ["bash", "-m", "start.sh"]
--- test/resources/ray_cartpole/train_ray.py
+++ test/resources/ray_cartpole/train_ray.py
@@ -5,19 +5,19 @@ import ray.rllib.agents.ppo as ppo
 from ray.tune.logger import pretty_print
 
 # Based on https://github.com/ray-project/ray/blob/master/doc/source/rllib-training.rst#python-api
-ray.init(log_to_driver=False)
+ray.init(log_to_driver=False, webui_host='127.0.0.1')
 config = ppo.DEFAULT_CONFIG.copy()
 config["num_gpus"] = int(os.environ.get("SM_NUM_GPUS", 0))
 checkpoint_dir = os.environ.get("SM_MODEL_DIR", '/Users/nadzeya/gym')
 config["num_workers"] = 1
-agent = ppo.PPOAgent(config=config, env="CartPole-v0")
+trainer = ppo.PPOTrainer(config=config, env="CartPole-v0")
 
 # Can optionally call agent.restore(path) to load a checkpoint.
 
 for i in range(5):
     # Perform one iteration of training the policy with PPO
-    result = agent.train()
+    result = trainer.train()
     print(pretty_print(result))
 
-    checkpoint = agent.save(checkpoint_dir=checkpoint_dir)
+    checkpoint = trainer.save(checkpoint_dir=checkpoint_dir)
     print("checkpoint saved at", checkpoint)
